19a8ccd1d80279d0f024b4038c3765effaf8f695
Rokon Khan <rokonjust@gmail.com>
2025-09-24 16:26:46 +0600

    Theme Provider Added

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,22 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-
-// import { Provider as ReduxProvider } from "react-redux";
-// import { RouterProvider } from "react-router";
-// import "./index.css";
-
-// import { Toaster } from "sonner";
-// import { ThemeProvider } from "./providers/ThemeProvider.tsx";
-// import { store } from "./redux/store.ts";
-// import router from "./routes/router.tsx";
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <ReduxProvider store={store}>
-//       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-//         <RouterProvider router={router} />
-//         <Toaster richColors position="top-center" />
-//       </ThemeProvider>
-//     </ReduxProvider>
-//   </StrictMode>
-// );
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router";
 import "./index.css";
-import router from "./routes/routes";
+
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./providers/ThemeProvider.tsx";
+import { store } from "./redux/store.ts";
+import router from "./routes/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ReduxProvider store={store}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <RouterProvider router={router} />
+        <Toaster richColors position="top-center" />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>
 );

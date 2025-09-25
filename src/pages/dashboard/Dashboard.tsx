75631801891dd968a9bce96f2057fc5654f05d16
Rokon Khan <rokonjust@gmail.com>
2025-09-25 17:19:03 +0600

    Dashboard Page Added

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,26 @@
+import AdminDashboard from "@/pages/dashboard/admin/AdminDashboard";
+import StaffDashboard from "@/pages/dashboard/staff/StaffDashboard";
+import SupplierDashboard from "@/pages/dashboard/supplier/SupplierDashboard";
+import { type RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+
+const Dashboard = () => {
+  const { user } = useSelector((state: RootState) => state.auth);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  switch (user.role) {
+    case "admin":
+      return <AdminDashboard />;
+    case "staff":
+      return <StaffDashboard />;
+    case "supplier":
+      return <SupplierDashboard />;
+    default:
+      return <div>Invalid role</div>;
+  }
+};
+
+export default Dashboard;

629d6f50f9162cd0ec26a70729779f940d4b238b
Rokon Khan <rokonjust@gmail.com>
2025-09-28 17:38:33 +0600

    Returns API RTK Query File Added

---
+++ src/redux/features/retruns/returnApi.ts
@@ -0,0 +1,28 @@
+import { baseApi } from "@/redux/baseApi";
+
+export const returnsApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    createReturn: build.mutation({
+      query: (body) => ({ url: "/returns", method: "POST", body }),
+      invalidatesTags: ["Returns"],
+    }),
+    getReturns: build.query({
+      query: () => "/returns",
+      providesTags: ["Returns"],
+    }),
+    updateReturn: build.mutation({
+      query: ({ id, ...body }) => ({
+        url: `/returns/${id}`,
+        method: "PATCH",
+        body,
+      }),
+      invalidatesTags: ["Returns"],
+    }),
+  }),
+});
+
+export const {
+  useCreateReturnMutation,
+  useGetReturnsQuery,
+  useUpdateReturnMutation,
+} = returnsApi;
